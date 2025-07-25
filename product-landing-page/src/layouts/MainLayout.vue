54bd53e86da0c174e724e537431bcd3d99e7b74c
Jason Rodrigues <jasonrodrigues28@gmail.com>
2025-07-25 13:47:21 +0530

    Made a dummy project to test

--- product-landing-page/src/layouts/MainLayout.vue
+++ product-landing-page/src/layouts/MainLayout.vue
@@ -1,9 +1,37 @@
 <template>
-  <router-view />
+  <q-layout view="hHh lpR fFf">
+    <!-- Header -->
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title>
+          Product Landing Page
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <!-- Optional Drawer -->
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+      <q-list>
+        <q-item clickable v-ripple to="/">
+          <q-item-section>Home</q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <!-- Main content area -->
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script>
 export default {
-  name: 'MainLayout'
+  name: 'MainLayout',
+  data() {
+    return {
+      leftDrawerOpen: false
+    }
+  }
 }
 </script>
