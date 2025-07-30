d44bcf66743db814a5c16265571d0d256b739f7f
SuyashGaude <suyashgaude04@gmail.com>
2025-07-30 18:38:40 +0530

    seller dashboard

---
+++ product-landing-page/src/components/SellerDashboard/ProductTable.vue
@@ -0,0 +1,66 @@
+<template>
+    <q-page class="q-pa-md">
+        <q-table 
+         :rows="products"
+         :columns="columns"
+         row-key="id"
+         flat
+         bordered
+         class="product-table">
+        <template v-slot:body-cell-actions="props">
+            <q-td>
+                <q-icon v-if="!deleteMode" name="edit" class="cursor-pointer"
+                @click="editProduct(props.row)"
+                v-show="props.row.hovered"
+                />
+                <q-icon v-else name="delete" class="cursor-pointer text-red"
+                @click="deleteProduct(props.row)"
+                v-show="props.row.hovered"
+                />
+                
+            </q-td>
+        </template>
+        </q-table>
+    </q-page>
+</template>
+<script>
+export default{
+    name: "ProductTable",
+    props:{
+        products: {
+            type: Array,
+            required: true
+        },
+        deleteMode: {
+            type: Boolean,
+            default: false
+        }
+    },
+     data() {
+    return {
+      columns: [
+        { name: "id", label: "Product ID", field: "id", align: "left" },
+        { name: "name", label: "Name", field: "name", align: "left" },
+        { name: "description", label: "Description", field: "description", align: "left" },
+        {
+          name: "hasDiscount",
+          label: "Discount?",
+          field: row => (row.hasDiscount ? "Yes" : "No"),
+          align: "center"
+        },
+        { name: "discountPercent", label: "Discount %", field: "discountPercent", align: "center" },
+        { name: "price", label: "Original Price", field: "price", align: "center" },
+        { name: "action", label: "", field: "action", align: "center" }
+      ]
+    };
+  },
+  methods:{
+    editProduct(){
+        console.log("Edit product clicked");
+    },
+    deleteProduct(){
+        console.log("deleteproduct clicked");
+    }
+  }
+}
+</script>
--- product-landing-page/src/pages/SellerDashboard.vue
+++ product-landing-page/src/pages/SellerDashboard.vue
@@ -1,2 +1,71 @@
-<template><q-page class="q-pa-md">Seller Dashboard</q-page></template>
-<script>export default { name: "SellerDashboard" };</script>
+<template>
+  <q-page class="q-pa-md">
+    <div class="sd-top-section row justify-end">
+      <q-btn
+        color="primary"
+        :icon-right="config.buttonTop.icon[0]"
+        :label="config.buttonTop.text[0]"
+        class="top-button"
+        @click="deleteMode = false"
+      />
+      <q-btn
+        color="red"
+        :icon-right="config.buttonTop.icon[1]"
+        :label="config.buttonTop.text[1]"
+        @click="deleteMode = true"
+      />
+    </div>
+
+    <div class="text-h6 q-mb-md">{{ config.heading.text[0] }}</div>
+
+    <!-- FIX: Changed from config.products to productList -->
+    <ProductTable :products="productList" :deleteMode="deleteMode" />
+  </q-page>
+</template>
+
+<script>
+import sellerDashboardConfig from "../configs/sellerdashboard.config.json";
+import ProductTable from "../components/SellerDashboard/ProductTable.vue";
+
+export default {
+  name: "SellerDashboard",
+  components: {
+    ProductTable
+  },
+  data() {
+    return {
+      config: sellerDashboardConfig,
+      deleteMode: false,
+      productList: [
+        {
+          id: "seller123-001",
+          name: "Product A",
+          description: "Some great product",
+          hasDiscount: true,
+          discountPercent: 10,
+          price: 1500,
+          hovered: false
+        },
+        {
+          id: "seller123-002",
+          name: "Product B",
+          description: "Another one",
+          hasDiscount: false,
+          discountPercent: 0,
+          price: 1000,
+          hovered: false
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.sd-top-section {
+  margin-bottom: 16px;
+}
+.top-button {
+  margin-right: 8px;
+}
+</style>
