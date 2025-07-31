cc05cf898cb9df6d2b89020d90ba47b5ddc9eb39
Jason Rodrigues <jasonrodrigues28@gmail.com>
2025-08-01 00:26:59 +0530

    Add cart icon with badge to navbar using Options API and Pinia

--- product-landing-page/src/layouts/MainLayout.vue
+++ product-landing-page/src/layouts/MainLayout.vue
@@ -3,6 +3,9 @@
     <q-header elevated class="bg-primary text-white">
       <q-toolbar>
         <q-toolbar-title>Product Landing Page</q-toolbar-title>
+        <q-btn flat round dense icon="shopping_cart" to="/cart">
+          <q-badge v-if="totalItems > 0" color="red" floating transparent :label="totalItems" />
+        </q-btn>
       </q-toolbar>
     </q-header>
 
@@ -13,7 +16,21 @@
 </template>
 
 <script>
+import { useCartStore } from 'stores/cart'
+
 export default {
-  name: "MainLayout"
-};
+  name: 'MainLayout',
+
+  data() {
+    return {
+      cartStore: useCartStore()
+    }
+  },
+
+  computed: {
+    totalItems() {
+      return this.cartStore.totalItems
+    }
+  }
+}
 </script>
