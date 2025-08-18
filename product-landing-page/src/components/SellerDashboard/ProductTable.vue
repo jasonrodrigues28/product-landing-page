bb72c4828d759ce74024813661e7b8ee9fff185e
Jason Rodrigues <jasonrodrigues28@gmail.com>
2025-08-18 17:10:59 +0530

    replaced hardcodedd code with json

--- product-landing-page/src/components/SellerDashboard/ProductTable.vue
+++ product-landing-page/src/components/SellerDashboard/ProductTable.vue
@@ -1,29 +1,19 @@
 <template>
   <div>
-    <q-table 
-      :rows="products" 
-      :columns="columns" 
-      row-key="productId"
-      :filter="filter"
-      :row-class="(row) => deleteMode && productsToDelete.has(row.productId) ? 'bg-red-1' : ''"
-    >
+    <q-table :rows="products" :columns="columns" row-key="productId" :filter="filter"
+      :row-class="(row) => deleteMode && productsToDelete.has(row.productId) ? 'bg-red-1' : ''">
       <template v-slot:top>
         <div class="row full-width">
           <div class="col-6">
-            <q-input
-              v-if="!deleteMode"
-              dense
-              debounce="300"
-              v-model="filter"
-              placeholder="Search products"
-            >
+            <q-input v-if="!deleteMode" dense debounce="300" v-model="filter" placeholder="Search products">
               <template v-slot:append>
                 <q-icon name="search" />
               </template>
             </q-input>
           </div>
           <div class="col-6 text-right">
-            <q-btn v-if="deleteMode" color="green" :label="config.buttons.save.label" @click="handleDeleteSave" class="q-mr-sm" />
+            <q-btn v-if="deleteMode" color="green" :label="config.buttons.save.label" @click="handleDeleteSave"
+              class="q-mr-sm" />
             <q-btn v-if="deleteMode" color="red" :label="config.buttons.cancel.label" @click="handleDeleteCancel" />
           </div>
         </div>
@@ -35,58 +25,35 @@
           <q-td key="name" :props="props">{{ props.row.name }}</q-td>
           <q-td key="description" :props="props">{{ props.row.description }}</q-td>
           <q-td key="hasDiscount" :props="props">{{ props.row.hasDiscount ? 'Yes' : 'No' }}</q-td>
-          <q-td key="discountPercent" :props="props">{{ props.row.hasDiscount ? props.row.discountPercent + '%' : '-' }}</q-td>
+          <q-td key="discountPercent" :props="props">{{ props.row.hasDiscount ? props.row.discountPercent + '%' : '-'
+          }}</q-td>
           <q-td key="originalPrice" :props="props">{{ props.row.originalPrice }}</q-td>
-          <q-td key="finalPrice" :props="props">{{ props.row.hasDiscount ? props.row.finalPrice : props.row.originalPrice }}</q-td>
+          <q-td key="finalPrice" :props="props">{{ props.row.hasDiscount ? props.row.finalPrice :
+            props.row.originalPrice }}</q-td>
           <q-td key="stock" :props="props" class="q-pa-md">
             <div v-if="props.row.colorVariants?.length > 0">
               <div v-for="color in props.row.colorVariants" :key="color" class="q-mb-sm">
                 <div class="text-caption">{{ color }}:</div>
-                <ProductQuantity
-                  :product-id="props.row.productId"
-                  :initial-stock="props.row.stockByColor?.[color] || 0"
-                  :is-seller="true"
-                  :selected-color="color"
-                  @stock-updated="onStockUpdated"
-                />
+                <ProductQuantity :product-id="props.row.productId" :initial-stock="props.row.stockByColor?.[color] || 0"
+                  :is-seller="true" :selected-color="color" @stock-updated="onStockUpdated" />
               </div>
             </div>
-            <ProductQuantity
-              v-else
-              :product-id="props.row.productId"
-              :initial-stock="props.row.stock || 0"
-              :is-seller="true"
-              @stock-updated="onStockUpdated"
-            />
+            <ProductQuantity v-else :product-id="props.row.productId" :initial-stock="props.row.stock || 0"
+              :is-seller="true" @stock-updated="onStockUpdated" />
           </q-td>
           <q-td key="createdAt" :props="props">{{ formatDate(props.row.createdAt) }}</q-td>
           <q-td key="actions" :props="props">
-            <q-btn 
-              v-if="!deleteMode"
-              flat 
-              round 
-              color="primary" 
-              :icon="config.icons.edit" 
-              @click="editProduct(props.row)" 
-            />
-            <q-btn
-              v-if="deleteMode"
-              flat
-              round
-              color="negative"
-              :icon="config.icons.delete"
-              @click="deleteProduct(props.row)"
-            />
+            <q-btn v-if="!deleteMode" flat round color="primary" :icon="config.icons.edit"
+              @click="editProduct(props.row)" />
+            <q-btn v-if="deleteMode" flat round color="negative" :icon="config.icons.delete"
+              @click="deleteProduct(props.row)" />
           </q-td>
         </q-tr>
       </template>
     </q-table>
 
     <q-dialog v-model="editDialog" persistent>
-      <EditProduct 
-        :product="productToEdit"
-        @close="closeEditDialog"
-      />
+      <EditProduct :product="productToEdit" @close="closeEditDialog" />
     </q-dialog>
   </div>
 </template>
@@ -104,7 +71,7 @@
 
 <script>
 import { useProductStore } from '../../stores/productStore';
-import productTableConfig from '../../configs/producttable.config.json';
+import appConfig from '../../configs/appConfig.json';
 import EditProduct from '../EditProduct.vue';
 import ProductQuantity from '../common/ProductQuantity.vue';
 
@@ -119,38 +86,12 @@ export default {
   },
   data() {
     return {
-      config: productTableConfig,
+      config: appConfig.productTable,
       filter: '',
       productsToDelete: new Set(),
       editDialog: false,
       productToEdit: null,
-      columns: [
-        { name: 'productId', label: 'Product ID', field: 'productId', align: 'left' },
-        { name: 'name', label: 'Name', field: 'name', align: 'left', sortable: true },
-        { name: 'description', label: 'Description', field: 'description', align: 'left' },
-        { name: 'hasDiscount', label: 'Discount?', field: row => row.hasDiscount ? 'Yes' : 'No', align: 'center' },
-        { name: 'discountPercent', label: 'Discount %', field: 'discountPercent', align: 'center' },
-        { name: 'originalPrice', label: 'Original Price', field: 'originalPrice', align: 'right', sortable: true },
-        { name: 'finalPrice', label: 'Final Price', field: 'finalPrice', align: 'right', sortable: true },
-        { 
-          name: 'stock', 
-          label: 'Stock Management', 
-          field: 'stock',
-          align: 'left',
-          sortable: true,
-          style: 'width: 200px',
-          classes: 'stock-cell'
-        },
-        { 
-          name: 'createdAt', 
-          label: 'Created', 
-          field: 'createdAt',
-          align: 'center',
-          sortable: true,
-          format: (val) => new Date(val).toLocaleString()
-        },
-        { name: 'actions', label: 'Actions', field: 'actions', align: 'center' }
-      ]
+      columns: appConfig.productTable.table.columns
     };
   },
   computed: {
